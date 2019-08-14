44d5a33110fe8f990c574df08021881b562900da
SUJIT <bhandarisujit@gmail.com>
2019-08-14 16:42:28 +0530

    Updates

---
+++ precache-manifest.8f912655deb30245683ee7e4feca1afd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036170e1d5ab6fe6bc55be5b3eaca502",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e243217736bb9bbae333",
+    "url": "/monsters/static/css/main.2bfef2ac.chunk.css"
+  },
+  {
+    "revision": "6b236ebb334ed0b328cd",
+    "url": "/monsters/static/js/2.ded042f5.chunk.js"
+  },
+  {
+    "revision": "e243217736bb9bbae333",
+    "url": "/monsters/static/js/main.98204057.chunk.js"
+  },
+  {
+    "revision": "ce1ae9d635e761e32001",
+    "url": "/monsters/static/js/runtime~main.3de43955.js"
+  }
+]);
